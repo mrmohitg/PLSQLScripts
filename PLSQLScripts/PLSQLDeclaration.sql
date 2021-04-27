5adb7807010e7eac5e5a734340f3812988116829
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-04-27 12:51:54 +0530

    Declaring Local Variable in PLSQL

--- PLSQLScripts/PLSQLDeclaration.sql
+++ PLSQLScripts/PLSQLDeclaration.sql
@@ -1,5 +1,12 @@
 DECLARE 
-
+ordernumber constant number:=101;
+orderid number default 100;
+customername varchar2(20):='Mohit';
 BEGIN
+orderid :=101;
+--ordernumber:=102; Because it is constant
 DBMS_OUTPUT.PUT_LINE('Welcome to PLSQL Course');
-END;
+DBMS_OUTPUT.PUT_LINE(orderid);
+DBMS_OUTPUT.PUT_LINE(ordernumber);
+DBMS_OUTPUT.PUT_LINE(customername);
+END; 
