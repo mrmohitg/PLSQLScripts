2b44c090fea6d3db7eb9a559e12a38936d211ddc
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-05-09 16:39:54 +0530

    VARRAY

---
+++ PLSQLScripts/38.PLSQLVARRAYS.sql
@@ -0,0 +1,24 @@
+DECLARE 
+TYPE customer_type IS VARRAY(4) OF VARCHAR2(100);
+ customer_table customer_type := customer_type();
+ v_idx NUMBER;
+BEGIN
+
+customer_table.EXTEND (4);
+customer_table(1) :='MIKE';
+customer_table(2) :='JEFF';
+customer_table(3) :='JOHN';
+--customer_table(6) :='MOHI';
+customer_table(4) :='KING';
+
+--customer_table.DELETE(3);
+
+v_idx := customer_table.FIRST;
+
+WHILE v_idx IS NOT NULL LOOP
+
+DBMS_OUTPUT.PUT_LINE('Customer Name ' || customer_table(v_idx));
+v_idx := customer_table.NEXT(v_idx);
+END LOOP display_loop;
+
+END;
