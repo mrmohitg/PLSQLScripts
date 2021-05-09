b1e3bcd54ab13ab04c60ab3ee61646e73c02e4d2
Mohit Gupta <mr.mohit.gupta.gi@gmail.com>
2021-05-09 20:33:26 +0530

    Bulk Processing 2

---
+++ PLSQLScripts/47.PLSQLBulkProcessing2.sql
@@ -0,0 +1,47 @@
+CREATE OR REPLACE PROCEDURE Check_Eligible (p_orderid NUMBER, l_eligible OUT BOOLEAN)
+AS
+l_total NUMBER;
+BEGIN
+
+SELECT SUM(total_amount) INTO l_total FROM sales
+WHERE order_id = p_orderid;
+
+IF l_total >= 1000 THEN
+    l_eligible := TRUE;
+
+END IF;
+END;
+
+
+CREATE OR REPLACE PROCEDURE Update_Tax1 (Tax_Rate IN NUMBER)
+AS
+l_eligible BOOLEAN;
+TYPE orderid_type IS TABLE OF sales.order_id%TYPE INDEX BY PLS_INTEGER;
+l_order_ids orderid_type;
+l_eligible_orders orderid_type;
+BEGIN
+
+SELECT DISTINCT order_id BULK COLLECT INTO l_order_ids
+FROM sales;
+
+FOR indx IN 1 .. l_order_ids.COUNT
+LOOP
+check_eligible(l_order_ids(indx), l_eligible);
+IF l_eligible
+THEN
+l_eligible_orders(l_eligible_orders.COUNT +1) :=l_order_ids (indx);
+END IF;
+
+END LOOP;
+
+FORALL INDX IN 1 .. l_eligible_orders.COUNT
+UPDATE sales s
+SET s.tax_amount = s.total_amount * tax_rate
+WHERE s.order_id = l_eligible_orders(INDX);
+
+COMMIT;
+END;
+
+exec Update_Tax1(.50);
+
+select * from sales;
